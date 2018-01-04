ebe11a01647f6e1edac52b2c842ab645592a827b
beautifularea <beautifularea@163.com>
2018-01-04 16:45:10 +0800

    my first shell script that is for CMake project

---
+++ make.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "************ Start to cmake ***********"
+cmake ..
+echo "************ End cmake ************"
+
+echo "************ Start to make ************"
+make
+echo "************ End make **************"
