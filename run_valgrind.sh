0ff2a469f0e01844c3d56a3d27ec1bd9b1b67d47
beautifularea <beautifularea@163.com>
2018-01-18 14:47:51 +0800

    Update run_valgrind.sh

--- run_valgrind.sh
+++ run_valgrind.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # t is executable program
 
+g++ -g -O0 a.cc -o t
 valgrind --tool=memcheck --log-file=all_ret.txt ./t
 valgrind --tool=memcheck --leak-check=yes --log-file=leak_ret.txt ./t
