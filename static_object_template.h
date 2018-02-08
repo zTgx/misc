e5468d15b779f26c68cc662825acff60bf057ec8
beautifularea <beautifularea@163.com>
2018-02-08 15:01:44 +0800

    Create static_object_template.h

---
+++ static_object_template.h
@@ -0,0 +1,58 @@
+template <typename T, typename Tag=void>
+class static_initializer
+{
+private:
+    T* inst_;
+
+public:
+    template <typename... Args>
+    explicit
+    static_initializer(Args&&... args)
+    {
+        static T t(std::forward<Args>(args)...);
+        inst_ = &t;
+    }
+
+    static_initializer()
+    {
+        static T t;
+        inst_ = &t;
+    }
+
+public:
+    T&
+    get() noexcept
+    {
+        return *inst_;
+    }
+
+    const T&
+    get() const noexcept
+    {
+        return const_cast<static_initializer&>(*this).get();
+    }
+
+    T&
+    operator*() noexcept
+    {
+        return get();
+    }
+
+    const T&
+    operator*() const noexcept
+    {
+        return get();
+    }
+
+    T*
+    operator->() noexcept
+    {
+        return &get();
+    }
+
+    const T*
+    operator->() const noexcept
+    {
+        return &get();
+    }
+};
