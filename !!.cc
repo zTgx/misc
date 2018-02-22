b11bee62802865f8c16da6b9cc36af1687c2160f
beautifularea <beautifularea@163.com>
2018-02-22 11:26:28 +0800

    Create !!.cc

---
+++ !!.cc
@@ -0,0 +1,19 @@
+//!! Usage.
+
+#include <iostream>
+
+int main()
+{
+    double a = .003;
+
+    std::cout << std::boolalpha;
+
+    std::cout << a << std::endl;
+
+    std::cout << !!a << std::endl;
+
+    for(int x=0; !!a; a=getA())
+    {
+        //todo
+    }
+}
