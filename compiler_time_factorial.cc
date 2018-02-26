e5d3da12121c719c26cabc7e2d61de031f1e3ec2
beautifularea <beautifularea@163.com>
2018-02-26 13:15:39 +0800

    编译期factorial

---
+++ compiler_time_factorial.cc
@@ -0,0 +1,15 @@
+// factorial as an integral_constant
+#include <iostream>
+#include <type_traits>
+
+template <unsigned n>
+struct factorial : std::integral_constant<int,n * factorial<n-1>::value> {};
+
+template <>
+struct factorial<0> : std::integral_constant<int,1> {};
+
+int main() {
+  std::cout << factorial<5>::value;  // constexpr (no calculations on runtime)
+  return 0;
+}
+http://www.cplusplus.com/reference/type_traits/integral_constant/
