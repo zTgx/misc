c04250b276d196a6dc41a984b9ab5a9a8971acf8
beautifularea <beautifularea@163.com>
2018-08-17 15:29:41 +0800

    Update 计算代码运行时间.cc

--- 计算代码运行时间.cc
+++ 计算代码运行时间.cc
@@ -3,4 +3,28 @@ timer t_ogm;
 ......
 some handle steps
 ......
-float time_cost = t_ogm.elapsed();//返回处理处理时间,秒
+float time_cost = t_ogm.elapsed();//返回处理处理时间
+
+
+
+//----------
+
+    const clock_t begin_time = clock();
+    int x = 0;
+
+    for(int i = 0; i<INT_MAX; i++)
+    {
+        if(i != -1)
+        {
+            x = 3;
+        }
+        else
+        {
+
+        }
+    }
+
+    float seconds = float(clock() - begin_time);
+    seconds = seconds/1000;
+    std::cout << "if use seconds : [" << seconds << "]" << std::endl;
+
